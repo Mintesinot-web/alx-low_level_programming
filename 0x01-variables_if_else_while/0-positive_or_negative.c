570353851f76d34d2b08ad4f37ff54acb2d29a62
Mintesinot <mintesinot@mintesinot.comgit config --global user.email mintesinot@mintesinot.com>
2022-04-11 02:00:06 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Prints a random number and states whether
- *          it is positive, negative, or zero.
+ *         it is positive, negative, or zero.
  *
  * Return: Always 0.
  */
@@ -15,9 +15,9 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-if (n>1)
+if (n > 1)
 printf("%d is positive\n", n);
-else if (n<0)
+else if (n < 0)
 printf("%d is negative\n", n);
 else
 
