7fdf63c2eee53c60acb7efc9a2a563f356a3c958
Mintesinot <mintesinot@mintesinot.comgit config --global user.email mintesinot@mintesinot.com>
2022-04-12 13:09:51 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - Prints the sign of a number.
- * @c: The number of which the sign will be printed.
+ * @n: The number of which the sign will be printed.
  *
  * Return: 1 if the number is greater than zero,
  *         0 if the number is zero,
@@ -13,7 +13,7 @@ int print_sign(int n)
 if (n > 0)
 {
 _putchar('+');
-return(1);
+return (1);
 }
 else if (n == 0)
 {
