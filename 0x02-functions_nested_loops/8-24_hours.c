ec85c462f8d21cf4c5538a3a472a6df9d6bae0ff
Mintesinot <mintesinot@mintesinot.comgit config --global user.email mintesinot@mintesinot.com>
2022-04-12 14:34:24 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
  * Jack_bauer - Prints every minute of the day of
- *              Jack Bauer, starting from 00:00 to 23:59.
+ *              Jack bauer, starting from 00:00 to 23:59.
  */
 void Jack_bauer(void)
 {
