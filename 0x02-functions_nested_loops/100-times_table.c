60ca525187b39cdb502fc829b16c1b8666dd03f6
Mintesinot <mintesinot@mintesinot.comgit config --global user.email mintesinot@mintesinot.com>
2022-04-13 11:24:28 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -38,7 +38,7 @@ _putchar((prod / 10) + '0');
 _putchar((prod / 10) + '0');
 }
 _putchar('\n');
-prod - num * mult;
+prod = num * mult;
  
 if (prod <= 99)
 _putchar(' ');
@@ -50,7 +50,8 @@ _putchar(',');
 _putchar((prod / 100) + '0');
 _putchar(((prod / 10)) % 10 + '0');
 }
-else (prod <= 99 && prod >= 10)
+else if (prod <= 99 && prod >= 10)
+
 {
 _putchar((prod / 10) + '0');
 }
