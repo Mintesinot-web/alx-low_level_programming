02bfbacfa9d8d283bc3228171a6880c7493fef57
Mintesinot <mintesinot@mintesinot.comgit config --global user.email mintesinot@mintesinot.com>
2022-04-12 23:06:27 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,31 +1,23 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * jack_bauer -prints every minute of the day
- *
- * Return: Always 0.
+ * jack_bauer - Prints every minute of the day of
+ *              Jack Bauer, starting from 00:00 to 23:59.
  */
-
-void jack_bauer(void);
-
+void jack_bauer(void)
 {
-int a;
-int b;
+  int hour, minute;
 
-for (a = 0; a <= 23; a++)
-{
-for (b = 0; b <= 59; b++)
-{
-_putchar((a / 10) + '0');
-_putchar((a % 10) + '0');
-_putchar(':');
-_putchar((b / 10) + '0');
-_putchar((b % 10) + '0');
-_putchar('\n');
-
-}
-
-
-}
-
+  for (hour = 0; hour <= 23; hour++)
+    {
+      for (minute = 0; minute <= 59; minute++)
+	{
+	  _putchar((hour / 10) + '0');
+	  _putchar((hour % 10) + '0');
+	  _putchar(':');
+	  _putchar((minute / 10) + '0');
+	  _putchar((minute % 10) + '0');
+	  _putchar('\n');
+	}
+    }
 }
