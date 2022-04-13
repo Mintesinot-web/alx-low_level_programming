40a9e8988d0f82e7e3a447be1871729b78b42565
Mintesinot <mintesinot@mintesinot.comgit config --global user.email mintesinot@mintesinot.com>
2022-04-13 01:34:35 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print all the numbers between the provided number and 98,
+ *               in order separated by a comma followed by a space.
+ * @n: integer to start at.
+ */
+void print_to_98(int n)
+{
+if (n > 98)
+{
+while (n > 98)
+printf("%d", i);
+}
+
+else
+{
+while (n < 98)
+printf("%d, ", n++);
+printf("%d\n", n);
+}
+}
