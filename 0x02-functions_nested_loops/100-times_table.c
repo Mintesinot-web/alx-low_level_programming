6f9df7ee05054d0358c31b4ce77d4e8fb3dc55c1
Mintesinot Ameha <101876808+Mintesinot-web@users.noreply.github.com>
2022-04-21 15:30:33 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,46 +7,46 @@ include "main.h"
  */
 void print_times_table(int n)
 {
-	int x = 0, y, z;
+int x = 0, y, z;
 
 
-	if (n > 15 || n < 0)
-		return; /**if the condtn above is satisfied, do NOTHING **/
-	while (x <= n)
-	{
-		for (y = 0; y <= n; y++)
-		{
-			z = x * y;
-			if (z > 99)
-			{
-				_putchar(z / 100 + '0');
-				_putchar((z / 10 % 10) + '0');
-				_putchar(z % 10 + '0');
-			}
-			else if (z > 9)
-			{
-				_putchar(' ');
-				_putchar(z / 10 + '0');
-				_putchar(z % 10 + '0');
-			}
-			else if (y != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(z + '0');
-			}
-			else
-				_putchar(z + '0');
+if (n > 15 || n < 0)
+return; /**if the condtn above is satisfied, do NOTHING **/
+while (x <= n)
+{
+for (y = 0; y <= n; y++)
+{
+z = x * y;
+if (z > 99)
+{
+_putchar(z / 100 + '0');
+_putchar((z / 10 % 10) + '0');
+_putchar(z % 10 + '0');
+}
+else if (z > 9)
+{
+_putchar(' ');
+_putchar(z / 10 + '0');
+_putchar(z % 10 + '0');
+}
+else if (y != 0)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(z + '0');
+}
+else
+_putchar(z + '0');
 
-			if (y != n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-		x++;
-	}
+if (y != n)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+x++;
+}
 }
 _putchar((prod / 10) + '0');
 }
