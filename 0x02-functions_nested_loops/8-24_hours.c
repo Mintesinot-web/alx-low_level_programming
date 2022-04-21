90d01d5b54b43fcfe8f27ffe9e1ab365f69392f4
Mintesinot Ameha <101876808+Mintesinot-web@users.noreply.github.com>
2022-04-21 15:20:05 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,26 +1,40 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints time
- *
- * Description: prints a list of time
- * Return: Always 0.
+ * jack_bauer - Print every minute of the day of Jack Bauer
+ * Description: Start from 00:00 to 23:59
  */
 
-main jack_bauer(void)
+void jack_bauer(void)
 {
-int i, j;
+int x = 0;
+int a = 0, b = 0, c = 0, d = 0;
 
-for (i = 0; i < 24; i++)
+while (x < 1440)
 {
-for (j = 0; j < 60; j++)
-{
-_putchar(i / 10 + 48);
-_putchar(i % 10 + 48);
+_putchar(a + '0');
+_putchar(b + '0');
 _putchar(':');
-_putchar(j / 10 + 48);
-_putchar(j % 10 + 48);
+_putchar(c + '0');
+_putchar(d + '0');
 _putchar('\n');
+
+d++;
+if (d > 9)
+{
+d = 0;
+c++;
 }
+if (c > 5)
+{
+c = 0;
+b++;
+}
+if (b > 9)
+{
+b = 0;
+a++;
+}
+x++;
 }
 }
